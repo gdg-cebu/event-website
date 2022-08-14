1cd5b11d2305593acc1c96526262ca6980cb1dc9
Arnelle Balane <arnellebalane@gmail.com>
2022-08-14 14:30:46 +0800

    Add template for Schedule page component

---
+++ src/pages/schedule.tsx
@@ -0,0 +1,22 @@
+import EventPageLayout from '../components/layouts/EventPageLayout';
+import SpacerSection from '../components/sections/SpacerSection';
+import DividerSection from '../components/sections/DividerSection';
+import SocialSection from '../components/sections/SocialSection';
+import * as types from '../../types';
+
+import type { NextPage } from 'next';
+
+const Schedule: NextPage = () => {
+  return (
+    <EventPageLayout
+      title="Schedule"
+      subtitle="Learn Google's latest developer products from Googlers, Google Developer Experts, guest speakers and more."
+    >
+      <SpacerSection size={types.SpacerSize.FLEX} />
+      <DividerSection />
+      <SocialSection />
+    </EventPageLayout>
+  );
+};
+
+export default Schedule;
