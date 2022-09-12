0b3040993e6054c1d858b0c8761ad4de94e9ab1f
Arnelle Balane <arnellebalane@gmail.com>
2022-09-13 01:56:57 +0800

    Add data-sb-* annotations to SpeakersSection component

--- src/components/sections/SpeakersSection.tsx
+++ src/components/sections/SpeakersSection.tsx
@@ -1,40 +1,54 @@
 import Image from 'next/image';
+import BaseSection from '../common/BaseSection';
 
 import type * as React from 'react';
 import type * as types from 'types';
 
-const SpeakersSection: React.FC<types.SpeakersSection> = (section) => {
+export type Props = types.SpeakersSection & types.StackbitAnnotation;
+
+const SpeakersSection: React.FC<Props> = (section) => {
   return (
-    <section className="py-8 px-6 md:py-20">
+    <BaseSection className="py-8 px-6 md:py-20" sb={section.sb}>
       <div className="xl:container mx-auto">
-        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" data-sb-field-path=".speakers">
           {section.speakers.map((speaker, index) => (
-            <article key={index} className="p-6 border border-complementary rounded text-center">
-              {renderImage(speaker)}
-              {renderDetails(speaker)}
-              {speaker.bio && (
-                <p className="mt-auto text-sm md:text-base text-copy-faded line-clamp-4">{speaker.bio}</p>
-              )}
-            </article>
+            <div key={index} data-sb-field-path={`.[${index}]`}>
+              <article
+                className="p-6 border border-complementary rounded text-center"
+                data-sb-object-id={(speaker as types.SourcebitObject).__metadata.id}
+              >
+                {renderImage(speaker)}
+                {renderDetails(speaker)}
+                {speaker.bio && (
+                  <p className="mt-auto text-sm md:text-base text-copy-faded line-clamp-4" data-sb-field-path=".bio">
+                    {speaker.bio}
+                  </p>
+                )}
+              </article>
+            </div>
           ))}
         </div>
       </div>
-    </section>
+    </BaseSection>
   );
 };
 
-const renderImage = ({ image }: types.Speaker): React.ReactNode => {
+const renderImage = (speaker: types.Speaker): React.ReactNode => {
+  const { image } = speaker;
   return (
     <div className="w-28 h-28 mx-auto mb-6 relative">
       {image ? (
-        <Image
-          src={image.url}
-          alt={image.alt}
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center center"
-          className="rounded-full"
-        />
+        <div data-sb-field-path=".image">
+          <Image
+            src={image.url}
+            alt={image.alt}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center center"
+            className="rounded-full"
+            data-sb-field-path=".url#@src .alt#@alt"
+          />
+        </div>
       ) : (
         <div className="w-full h-full rounded-full bg-complementary-faded" />
       )}
@@ -42,15 +56,18 @@ const renderImage = ({ image }: types.Speaker): React.ReactNode => {
   );
 };
 
-const renderDetails = ({ name, designation, company }: types.Speaker): React.ReactNode => {
+const renderDetails = (speaker: types.Speaker): React.ReactNode => {
+  const { name, designation, company } = speaker;
   return (
     <>
-      <h2 className="text-lg md:text-xl">{name}</h2>
+      <h2 className="text-lg md:text-xl" data-sb-field-path=".name">
+        {name}
+      </h2>
       {(designation || company) && (
         <p className="mb-6 text-sm md:text-base text-copy-faded">
-          {designation}
+          {designation && <span data-sb-field-path=".designation">{designation}</span>}
           {designation && company && ', '}
-          {company}
+          {company && <span data-sb-field-path=".company">{company}</span>}
         </p>
       )}
     </>
