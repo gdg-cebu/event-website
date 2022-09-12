2a2c6a62e37635af3ace0ffa7d53f92b8942894f
Arnelle Balane <arnellebalane@gmail.com>
2022-09-13 01:12:51 +0800

    Add data-sb-* annotations to GallerySection component

--- src/components/sections/GallerySection.tsx
+++ src/components/sections/GallerySection.tsx
@@ -1,40 +1,50 @@
 import Image from 'next/image';
 import cloneDeep from 'lodash/cloneDeep';
+import BaseSection from '../common/BaseSection';
 import Button from '../common/Button';
 import styles from './GallerySection.module.css';
 
 import type * as React from 'react';
 import type * as types from 'types';
 
-const GallerySection: React.FC<types.GallerySection> = (section) => {
-  const data = prepareSectionData(section);
+export type Props = types.GallerySection & types.StackbitAnnotation;
 
+const GallerySection: React.FC<Props> = (section) => {
   return (
-    <section className="p-1">
+    <BaseSection className="p-1" sb={section.sb}>
       <div className={styles.grid}>
-        {renderContent(data)}
-        {renderImages(data)}
+        {renderContent(section)}
+        {renderImages(section)}
       </div>
-    </section>
+    </BaseSection>
   );
 };
 
 const renderContent = ({ title, description, cta }: types.GallerySection): React.ReactNode => {
   return (
     <div className={`flex flex-col items-start py-12 px-6 md:px-12 -mx-1 md:mx-0 ${styles.content}`}>
-      <h2 className="mb-4 text-xl md:text-2xl font-bold">{title}</h2>
-      {description && <p className="mb-4 text-base md:text-lg">{description}</p>}
+      <h2 className="mb-4 text-xl md:text-2xl font-bold" data-sb-field-path=".title">
+        {title}
+      </h2>
+      {description && (
+        <p className="mb-4 text-base md:text-lg" data-sb-field-path=".description">
+          {description}
+        </p>
+      )}
       {cta && (
-        <Button href={cta.url} className="mt-auto">
-          {cta.label}
-        </Button>
+        <div className="inline-block mt-auto" data-sb-field-path=".cta">
+          <Button href={cta.url} sb=".label .url#@href">
+            {cta.label}
+          </Button>
+        </div>
       )}
     </div>
   );
 };
 
 const renderImages = ({ images }: types.GallerySection): React.ReactNode => {
-  if ((images?.length ?? 0) === 0) {
+  const data = prepareCompleteImages(images);
+  if (data.length === 0) {
     return (
       <>
         {new Array(9).fill(0).map((_, index) => (
@@ -45,24 +55,34 @@ const renderImages = ({ images }: types.GallerySection): React.ReactNode => {
   }
   return (
     <>
-      {images.map((image, index) => (
-        <div key={index} className={`w-full h-full bg-complementary-faded relative ${styles.item}`}>
-          <Image src={image.url} alt={image.alt} layout="fill" objectFit="cover" objectPosition="center center" />
+      {data.map((image, index) => (
+        <div
+          key={index}
+          className={`w-full h-full bg-complementary-faded relative ${styles.item}`}
+          data-sb-field-path={index < images.length ? `.images.[${index}]` : null}
+        >
+          <Image
+            src={image.url}
+            alt={image.alt}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center center"
+            data-sb-field-path={index < images.length ? '.url#@src .alt#@alt' : null}
+          />
         </div>
       ))}
     </>
   );
 };
 
-const prepareSectionData = (section: types.GallerySection): types.GallerySection => {
-  const data = cloneDeep(section);
-  if ((data.images?.length ?? 0) > 0) {
-    let images: types.Image[] = [];
-    while (images.length < 9) {
-      images = [...images, ...data.images];
+const prepareCompleteImages = (images: types.Image[]): types.Image[] => {
+  let data: types.Image[] = [];
+  const validImages = images.filter((image) => image.url);
+  if (validImages.length > 0) {
+    while (data.length < 9) {
+      data = [...data, ...validImages];
     }
-    images = images.slice(0, 9);
-    data.images = images;
+    data = data.slice(0, 9);
   }
   return data;
 };
